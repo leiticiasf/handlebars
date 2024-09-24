91710847abb5148433f5360199a5049bd0776b81
LETÍCIA SOARES FERREIRA <leticia_s_ferreira@estudante.sesisenai.org.br>
2024-09-24 12:32:55 -0300

    handlebars

---
+++ handlebars/instalacao/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/', function (req, res){
+    res.render('home', {layout: false})
+})
+
+app.listen(3000, () =>{
+    console.log('App funfando')
+})
